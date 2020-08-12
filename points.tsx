2172762e7b9124aec33fbd76c3c244ebb9f88d6e
Udi Oron <udioron@gmail.com>
2020-08-12 11:05:03 +0300

    Added simple method

--- points.tsx
+++ points.tsx
@@ -1,13 +1,17 @@
 class Point {
-    x = 0;
+    x = 0;   // property
     y = 0;
     color = "red";
+
+    distance() {
+        return this.x + this.y;
+    }
 }
 
 const s1 = "shalom";
 const s2 = "bye";
 
-const p1 = new Point()
+const p1 = new Point()  // p1 is an instance of Point
 console.log(p1);
 
 const p2 = new Point()
@@ -16,8 +20,9 @@ console.log(p2);
 p1.x = 100;
 p1.y = 200;
 console.log(p1);
-console.log(p1.x);
+console.log(p1.x);  // p1.x is an instance variable
 console.log(p1.color);
+console.log(p1.distance());
 
 console.log("done.")
 
