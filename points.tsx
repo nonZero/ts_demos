53e7700c6f8ed1a665fa6c1226ec3b7fd3e8def1
Udi Oron <udioron@gmail.com>
2020-08-12 10:56:43 +0300

    very basic typescript class demo

---
+++ points.tsx
@@ -0,0 +1,21 @@
+class Point {
+    public x = 0;
+    public y = 0;
+}
+
+const s1 = "shalom";
+const s2 = "bye";
+
+const p1 = new Point()
+console.log(p1);
+
+const p2 = new Point()
+console.log(p2);
+
+p1.x = 100;
+p1.y = 200;
+console.log(p1);
+
+console.log("done.")
+
+
